cb5faec9490a098cad65b1b814f014bff524f4a5
Benjy Rosen <858324@students.wits.ac.za>
2018-05-13 12:46:52 +0200

    Added function to loop through file and get values of specific columns

--- test.c
+++ test.c
@@ -5,52 +5,51 @@
 #include <string.h>
 #include <glib.h>
 
-int main( int argc, char *argv[] )
+void HashFile(char* fileName, int hashColumn)
 {
   FILE *file;
-  char *line = NULL;
+  gchar *line = NULL;
   size_t len = 0;
   int pipeChar = 124;
 
-  file = fopen("2.17.3/ref_data/1/customer.tbl.1", "rb");
+  file = fopen(fileName, "rb");
 
   if (file == NULL)
         exit(1);
-  GArray *lines = g_array_new(FALSE,FALSE,sizeof(line));
+  GPtrArray *lines;
+  // GHashTable *hashTable = g_hash_table_new(g_str_hash, g_str_equal);
+  lines = g_ptr_array_new();
   while (getline(&line, &len, file) != -1) {
     // printf("%s\n", line);
     int startIndex = 0;
     int endIndex = 0;
     int colNum = 0;
-    g_array_append_val(lines,line);
-    do {
-      if (line[endIndex] == pipeChar) {
-        if (startIndex != 0 )
-        {
-          printf("Found in col %i: %.*s\n", colNum, (endIndex - startIndex) - 1, line + startIndex + 1);
-        }
-        else
-        {
-          printf("Found in col %i: %.*s\n", colNum, endIndex - startIndex, line + startIndex);
-        }
-        colNum++;
-        startIndex = endIndex;
-        // strcpy(input, "");
-      }
-      else {
-        // strcat(input, line[index]);
-        // printf("%s\n", &line[index]);
-      }
-      endIndex++;
-    } while(strcmp(&line[endIndex], "\n") != 0);
-    printf("\n");
-  }
+    g_ptr_array_add(lines, (gpointer)g_strdup(line));
 
+    char *colContent = strtok(line, "|");
+    for (int i = 1; i <= hashColumn; i++)
+    {
+      colContent = strtok(NULL, "|");
+      if (colContent == NULL || strcmp(colContent, "\n") == 0)
+      {
+        fprintf(stderr, "Error: Column %i out of range\n", hashColumn);
+        exit(EXIT_FAILURE);
+      }
+    }
+    // We are at column to hash
+    printf("%s\n", colContent);
+  }
   free(line);
   fclose(file);
-  char *testVal = g_array_index(lines,char*,1);
-  printf(testVal);
-  g_array_free(lines,TRUE);
+}
+
+int main( int argc, char *argv[] )
+{
+
+  char* fileName = "2.17.3/ref_data/1/customer.tbl.1";
+  HashFile(fileName, 7);
+  // g_print("Hello %s\n", g_ptr_array_index(lines,0));
+  // g_ptr_array_free(lines,TRUE);
   return 0;
 /*** Select one of the following
     MPI_Init_thread( 0, 0, MPI_THREAD_SINGLE, &provided );
